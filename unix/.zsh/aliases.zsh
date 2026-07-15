a4e81eefd6dbfa9c66b857fdf7833e5be2f20f87
Josh <jny986@gmail.com>
2026-07-15 16:21:23 +0930

    Fix for open function breaking MacOS Open

--- unix/.zsh/aliases.zsh
+++ unix/.zsh/aliases.zsh
@@ -4,11 +4,10 @@
 
 # Laravel
 alias sail='[ -f sail ] && sail || vendor/bin/sail'
-alias a="artisan"
-alias tinker="artisan tinker"
-alias seed="artisan db:seed"
-alias serve="artisan serve"
-alias mfs="artisan migrate:fresh --seed"
+alias art="php artisan"
+alias tinker="php artisan tinker"
+alias seed="php artisan db:seed"
+alias mfs="php artisan migrate:fresh --seed"
 alias sqlit="sed -e 's/\(DB_.*\)/# \\1/g' -e 's/# \(DB_CONNECTION=\).*/\\1sqlite/g' -i .env"
 alias lenv="cp -n .env.example .env && (grep '^APP_KEY=.\+' .env > /dev/null || artisan key:generate)"
 alias laravel-setup="composer install && lenv && sqlit && artisan migrate --force --seed"
@@ -26,3 +25,8 @@ alias dc="docker compose"
 alias dd="docker compose down"
 alias dup="docker compose up -d"
 alias dre="docker compose down && docker compose up -d"
+
+# Python
+alias python="python3.14"
+alias py="python3.14"
+alias pip="pip3.14"
--- unix/.zsh/functions.zsh
+++ unix/.zsh/functions.zsh
@@ -11,10 +11,6 @@ de () {
     docker compose exec -it $1 ${2-bash}
 }
 
-open () {
-    xdg-open $* > /dev/null 2>&1
-}
-
 composer-link() {
     composer config minimum-stability dev
     local package=`echo $1 | sed -nr 's/.*\/([^\/]+)$/\1/p'`
@@ -26,4 +22,3 @@ composer-github() {
     local package=`echo $1 | sed -nr 's/.*\/(.*)\.git/\1/p'`
     composer config "repositories.$package" vcs $1
 }
-
